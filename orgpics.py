23c46a230a3b1c635ea5bc68152f9ba11522fac4
Alok Singhal <gandalf013@gmail.com>
2014-04-22 20:49:38 -0700

    raw files go in dated folders; camera db gone
    I wasn't using camera db stuff so I removed it.
    
    Raw files now go into a sub-folder of the dated folder.  This makes it
    easy to access the raw files when processing files later.

--- orgpics.py
+++ orgpics.py
@@ -6,7 +6,6 @@ import os
 import optparse
 import shutil
 import itertools
-import shlex
 import hashlib
 
 import pyexiv2
@@ -15,10 +14,9 @@ DATETIME_KEYS = (
     'Exif.Photo.DateTimeOriginal',
     'Exif.Image.DateTimeOriginal',
     'Exif.Photo.DateTimeDigitized',
-    # 'Exif.Image.DateTime'
 )
 
-CAMERA_KEY = 'Exif.Image.Model'
+RAW_EXTENSIONS = set(['.cr2', '.nef'])
 
 def make_unique_filename(name):
     for i in itertools.count():
@@ -26,123 +24,88 @@ def make_unique_filename(name):
         if not os.path.exists(test_name):
             return test_name
 
-class CameraDB(dict):
-    def __init__(self, options):
-        self.options = options
-        self.filename = options.cfg_filename
-        self.read_db()
-
-    def read_db(self):
-        try:
-            fp = open(self.filename)
-        except IOError, e:
-            logging.debug('cannot read db file: %s: %s' % (self.filename, e))
-            return
-
-        for i, line in enumerate(fp):
-            data = line.strip()
-            if not data or data.startswith('#'):
-                continue
-
-            tokens = shlex.split(data, comments=True)
-            if len(tokens) == 3 and tokens[1] == '=':
-                self[tokens[0]] = tokens[2]
-            else:
-                key, eq, value = data.partition('=')
-                if eq != '=':
-                    logging.warning('%d: invalid line: %s' % (i+1, line))
-                    continue
-                value = shlex.split(value, comments=True)[0]
-                self[key] = value
-
-        fp.close()
-
 def real_name(path):
     return os.path.abspath(os.path.normpath(path))
+
+def is_raw_file(path):
+    _, ext = os.path.splitext(path)
+    return ext.lower() in RAW_EXTENSIONS
 
 def same_file(file1, file2, thorough=True):
     s1 = os.stat(file1)
     s2 = os.stat(file2)
-    if (s1.st_size == s2.st_size):
-        if thorough:
-            m1 = hashlib.md5()
-            m2 = hashlib.md5()
-            m1.update(open(file1).read())
-            m2.update(open(file2).read())
-            return m1.digest() == m2.digest()
-        else:
-            return True
-    else:
+
+    if s1.st_size != s2.st_size:
         return False
 
-def process_file(filename, options, db):
-    logging.debug('%s...' % filename)
+    if not thorough:
+        return True
+
+    m1 = hashlib.md5()
+    m2 = hashlib.md5()
+    with open(file1) as fp1:
+        m1.update(fp1.read())
+    with open(file2) as fp2:
+        m2.update(fp2.read())
+
+    return m1.digest() == m2.digest()
+
+def process_file(filename, options):
+    logging.debug('%s...', filename)
     try:
         meta = pyexiv2.ImageMetadata(filename)
     except UnicodeDecodeError, e:
-        logging.info('bad filename: %s, not processing' % filename)
+        logging.info('bad filename: %s, not processing', filename)
         return
 
     try:
         meta.read()
     except IOError, e:
-        # logging.warning('error processing %s: %s' % (filename, e))
+        # logging.warning('error processing %s: %s', filename, e)
         return
 
     for key in DATETIME_KEYS:
         try:
             date_time =  meta[key].value
             if isinstance(date_time, str):
-                logging.warning('%s: bad date/time: %s' % (filename, date_time))
+                logging.warning('%s: bad date/time: %s', filename, date_time)
             else:
                 break
         except KeyError:
             continue
     else:
-        logging.warning('%s: no date' % filename)
+        logging.warning('%s: no date', filename)
         date_time = None
-
-    if options.use_camera:
-        try:
-            camera = meta[CAMERA_KEY].value
-        except KeyError:
-            logging.warning('%s: no camera' % filename)
-            camera = None
-    else:
-        camera = None
 
     base_name = os.path.basename(filename)
     out_dir = options.out_dir
-    if camera is not None:
-        camera = camera.replace(' ', '_')
-        camera = camera.replace(',', '.')
-        camera = camera.strip('_.')
-        if camera in db:
-            logging.info('camera %s -> %s' % (camera, db[camera]))
-            camera = db[camera]
-        out_dir = os.path.join(out_dir, camera)
 
     if date_time is not None:
         date_str = date_time.strftime(options.fmt)
-        out_filename = os.path.join(out_dir, date_str, base_name)
+        out_dir = os.path.join(out_dir, date_str)
     else:
-        out_filename = os.path.join(out_dir, 'NoDate', base_name)
+        out_dir = os.path.join(out_dir, 'NoDate')
+
+    is_raw = is_raw_file(filename)
+    if is_raw:
+        out_dir = os.path.join(out_dir, 'Raw')
+
+    out_filename = os.path.join(out_dir, base_name)
 
     act = True
     if real_name(out_filename) == real_name(filename):
-        logging.info('%s and %s are the same file, nothing to do' %
-                (out_filename, filename))
+        logging.info('%s and %s are the same file, nothing to do',
+                     out_filename, filename)
         act = False
     elif os.path.exists(out_filename):
         if same_file(out_filename, filename, thorough=False):
-            logging.info('ignoring %s, already processed' % filename)
+            logging.info('ignoring %s, already processed', filename)
             return
         if not options.overwrite:
             out_filename = make_unique_filename(out_filename)
 
-    out_dir = os.path.dirname(out_filename)
     if not os.path.exists(out_dir):
-        logging.info('making directory: %s' % out_dir)
+        logging.info('making directory: %s', out_dir)
         os.makedirs(out_dir)
 
     if act:
@@ -154,30 +117,29 @@ def process_file(filename, options, db):
             op = 'move'
 
         if not options.dry_run:
-            logging.debug('%s %s -> %s' % (op, filename, out_filename))
+            logging.debug('%s %s -> %s', op, filename, out_filename)
             try:
                 func(filename, out_filename)
             except IOError, e:
-                logging.info('%s: error: %s' % (op, e))
+                logging.info('%s: error: %s', op, e)
         else:
-            logging.info('%s %s -> %s' % (op, filename, out_filename))
+            logging.info('%s %s -> %s', op, filename, out_filename)
 
-def process_dir(dirname, options, db):
+def process_dir(dirname, options):
     for root, dirs, files in os.walk(dirname):
         for fname in files:
             filename = os.path.join(root, fname)
-            process_file(filename, options, db)
+            process_file(filename, options)
 
 def run(options, args):
-    db = CameraDB(options)
     for name in args:
         if os.path.isdir(name):
             if options.recurse:
-                process_dir(name, options, db)
+                process_dir(name, options)
             else:
-                logging.info('ignoring directory %s' % name)
+                logging.info('ignoring directory %s', name)
         elif os.path.isfile(name):
-            process_file(name, options, db)
+            process_file(name, options)
         else:
             raise ValueError('invalid argument %s' % name)
 
@@ -196,34 +158,27 @@ def main(args=None):
     usage = 'usage: %prog [options] <path(s)>'
     parser = optparse.OptionParser(usage=usage)
     parser.add_option('-D', '--debug', dest='debug', action='store_true',
-            default=False, help='debug')
+                      default=False, help='debug')
     parser.add_option('-d', '--out-dir', dest='out_dir', default=None,
-            metavar='DIR', help='output directory=DIR')
+                      metavar='DIR', help='output directory=DIR')
     parser.add_option('-f', '--fmt', dest='fmt', default='%Y-%m-%d',
-            metavar='FMT', help='strftime() format for output directory')
+                      metavar='FMT',
+                      help='strftime() format for output directory')
     parser.add_option('-r', '--recurse', dest='recurse', action='store_true',
-            default=False, help='recurse directories')
+                      default=False, help='recurse directories')
     parser.add_option('-o', '--overwrite', dest='overwrite',
-            action='store_true', default=False,
-            help='overwrite files (default=False: add an extension)')
+                      action='store_true', default=False,
+                      help='overwrite files (default=False: add an extension)')
     parser.add_option('-c', '--copy', dest='copy', action='store_true',
-            default=False, help='copy files instead of moving')
+                      default=False, help='copy files instead of moving')
     parser.add_option('-n', '--dry-run', dest='dry_run', action='store_true',
-            default=False, help='dry run')
-    parser.add_option('-F', '--cfg-file', dest='cfg_filename', default=None,
-            metavar='FILE', help='config file')
-    parser.add_option('-C', '--no-camera', dest='use_camera',
-            action='store_false', default=True,
-            help="don't create separate directories for each camera")
+                      default=False, help='dry run')
 
     options, args = parser.parse_args(args)
 
     home = os.environ['HOME']
     if options.out_dir is None:
         options.out_dir = os.path.join(home, 'OrganizedPics')
-
-    if options.cfg_filename is None:
-        options.cfg_filename = os.path.join(home, '.camera_db.rc')
 
     setup_logging(options.debug)
 
@@ -234,6 +189,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     sys.exit(main())
-
-# vim:ft=python
-
